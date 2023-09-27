7795ee8fa289bb4156f3e342765f5e2583216770
Никита Вахрушев <n332303@gmail.com>
2023-09-27 21:06:11 +1000

    adding files

--- src/Guessing.java
+++ src/Guessing.java
@@ -4,78 +4,55 @@ import java.util.Scanner;
 /**
  * Тут, короче, наскоряк накиданная клёвая игра в угадайку.
  * Оно число загадывает, а пользователь потом угадывает.
- * Есть подсказки, насколько далеко.
+ * Есть подсказки, на сколько далеко.
  * todo: DRY, KISS, SOLID (точно можно на ООП-шить), и опечатки по мелочи
  */
 public class Guessing {
 
-    public static int randomiser(int r1, int r2){
-        return new Random().nextInt(r1, r2) + 1;
-    }
-
-    public static void range(int number, int rand){
-
-        if (number < 1 || number > 10) {
-            System.out.println("Читать не умеешь?");
-        } else if (Math.abs(number - rand) > 5) {
-            System.out.println("Холодно");
-        } else if (Math.abs(number - rand) > 2) {
-            System.out.println("Тепло");
-        } else {
-            System.out.println("Жгётся!");
-        }
-    }
-
-    public static void gsr1(int rand){
-        while (true) {
-            Scanner sc = new Scanner(System.in);
-            int number = sc.nextInt();
-            if (number == rand) {
-                break;
-            }
-            else {
-                range(number, rand);
-            }
-        }
-    }
-
-
-    public static boolean dialog(int rand){
-        System.out.println("╰(▔∀▔)╯\nБудешь угадывать? (да/нет)");
-        Scanner sc1 = new Scanner(System.in);
-        String response = sc1.nextLine();
-        switch (response) {
-            case "нет": {
-                System.out.println("(¬_¬ )");
-                return false;
-            }
-            case "да": {
-                while (true) {
-                    System.out.println("(⌒‿⌒)\nугадай число от 1 до 10");
-                    gsr1(rand);
-                    return true;
-                }
-            }
-            default: {
-                System.out.println("(︶︹︺)\n непонятно, давай до свидания");
-                return false;
-            }
-        }
-    }
     public static void main(String[] args) {
-
-        System.out.println("Привет!");
-        try {
-            while (true){
-                if (!dialog(randomiser(0, 10))){
+        System.out.println("Привет!\nБудешь угадывать? (да/нет)");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.nextLine();
+        if (answer.equals("нет")) {
+            System.out.println("(×﹏×)");
+            return;
+        } else if (!answer.equals("да")) {
+            System.out.println("(︶︹︺)\n непонятно, давай до свидания");
+            return;
+        }
+        System.out.println("(⌒‿⌒)");
+        while (true) {
+            int rand = new Random().nextInt(0, 10) + 1;
+            System.out.println("угадай число от 1 до 10");
+            while (true) {
+                int number = sc.nextInt();
+                if (number == rand) {
+                    System.out.println("╰(▔∀▔)╯");
+                    System.out.println("Будешь угадывать? (да/нет)");
+                    sc = new Scanner(System.in);
+                    answer = sc.nextLine();
+                    if (answer.equals("нет")) {
+                        System.out.println("(¬_¬ )");
+                        return;
+                    } else if (!answer.equals("да")) {
+                        System.out.println("(︶︹︺)\n непонятно. Давай, до свидания!");
+                        return;
+                    }
+                    System.out.println("(⌒‿⌒)");
                     break;
+                } else {
+                    if (number < 1 || number > 10) {
+                        System.out.println("Читать не умеешь?");
+                    } else if (Math.abs(number - rand) > 5) {
+                        System.out.println("Холодно");
+                    } else if (Math.abs(number - rand) > 2) {
+                        System.out.println("Тепло");
+                    } else {
+                        System.out.println("Жгётся!");
+                    }
                 }
             }
         }
-        catch (Exception exception){
-            System.out.println("Было введено что-то не то! "+exception);
-        }
-
     }
 
 }
